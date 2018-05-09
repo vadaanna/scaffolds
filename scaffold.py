5c91c02125cdab66abc130d7966db0dd0a27d2d3
Anna Gustafsson <Anna@edu221236.eduroam.gu.se>
2018-05-09 11:36:48 +0200

    second commit

--- scaffold.py
+++ scaffold.py
@@ -4,12 +4,15 @@ Spyder Editor
 
 This is a temporary script file.
 """
-If proteins (y) >1 in samples =>10 then:
+#Read data from file
+If proteins ratio (y) >1. in samples (x) =>10 then:
 	List proteins 
 Print list of proteins 
 
+
 If protein >2 then list proteins
 Print list of proteins 
+
 
 Output list of highest expressed proteins per sample
 	List proteins
