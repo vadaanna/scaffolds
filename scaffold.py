76a91c4e474928d926d2851b4b3f54e629afaba0
Anna Gustafsson <Anna@edu221236.eduroam.gu.se>
2018-05-09 10:24:08 +0200

    First commit

---
+++ scaffold.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+If proteins (y) >1 in samples =>10 then:
+	List proteins 
+Print list of proteins 
+
+If protein >2 then list proteins
+Print list of proteins 
+
+Output list of highest expressed proteins per sample
+	List proteins
+Print list
+
+
